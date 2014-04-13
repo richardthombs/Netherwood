01cf0eca6d8c493a82033b5af2850b937997f0b0
Richard Thombs <richard@teamhaven.com>
2014-04-13 16:59:25 +0100

    Reproduction

--- Assets/MainObject.cs
+++ Assets/MainObject.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 public class MainObject : MonoBehaviour
 {
+    public int FoodCount=500;
+    public int MobCount=1;
+
     static System.Random rnd = new System.Random();
     List<GameObject> mobs = new List<GameObject>();
     List<GameObject> food = new List<GameObject>();
@@ -10,11 +13,11 @@ public class MainObject : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        var mobPrefab = GameObject.Find("mobPrefab");
-        CloneMob(mobPrefab, 5000);
+        var mobPrefab = GameObject.Find("Mobile");
+        CloneMob(mobPrefab, MobCount - 1);
 
-        var foodPrefab = GameObject.Find("foodPrefab");
-        for (int i = 0; i < 1000; i++)
+        var foodPrefab = GameObject.Find("Food");
+        for (int i = 0; i < FoodCount - 1; i++)
         {
             var obj = (GameObject)Instantiate(foodPrefab);
             obj.transform.position = RandomPosition(0);
@@ -35,9 +38,11 @@ public class MainObject : MonoBehaviour
             obj.renderer.material.color = new Color((float)rnd.NextDouble(), (float)rnd.NextDouble(), (float)rnd.NextDouble());
 
             var m = obj.GetComponent<Mobile>();
-            m.Health = rnd.Next(1, 10) * 10;
-            m.Energy = rnd.Next(1, 10) * 10;
+            m.Body.Health = rnd.Next(1, 10) * 10;
             m.Destination = RandomPosition(1f);
+
+            m.Reproduction.Sex = (Sex)rnd.Next(3);
+            m.Reproduction.Species = obj.renderer.material.color;
 
             mobs.Add(obj);
         }
--- Assets/Mobile.cs
+++ Assets/Mobile.cs
@@ -1,46 +1,199 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public class Stomach
+{
+    public float Capacity;
+    public float Contents;
+
+    public float Digest(float qty)
+    {
+        var digested = Mathf.Min(qty, Contents);
+        Contents -= digested;
+        return digested;
+    }
+
+    public float Ingest(float qty)
+    {
+        var ingested = Mathf.Min(Capacity - Contents, qty);
+        Contents += ingested;
+        return ingested;
+    }
+}
+
+[System.Serializable]
+public class FatReserve
+{
+    public float Capacity;
+    public float Contents;
+
+    public float Get(float qty)
+    {
+        var digested = Mathf.Min(qty, Contents);
+        Contents -= digested;
+        return digested;
+    }
+
+    public float Put(float qty)
+    {
+        var ingested = Mathf.Min(Capacity - Contents, qty);
+        Contents += ingested;
+        return ingested;
+    }
+}
+
+[System.Serializable]
+public class Metabolism
+{
+    public float FoodConversionRate;
+    public float FatConversionRate;
+    public float BurnRate;
+}
+
+[System.Serializable]
+public class Body
+{
+    public float Health;
+
+    public Stomach Stomach;
+    public FatReserve Fat;
+    public Metabolism Metabolism;
+
+    public bool Hungry;
+    public bool Dead;
+
+    public void Tick(float elapsed)
+    {
+        float energyRequired = Metabolism.BurnRate * elapsed;
+
+        // Digest our food
+        var foodConverted = Metabolism.FoodConversionRate * elapsed;
+        var energyFromFood = Stomach.Digest(foodConverted);
+        energyRequired -= energyFromFood;
+
+        // If we have an energy shortfall, then we digest some fat too
+        if (energyRequired > 0)
+        {
+            var fatConverted = Metabolism.FatConversionRate * elapsed;
+            var energyFromFat = Fat.Get(fatConverted);
+            energyRequired -= energyFromFat;
+        }
+
+        if (energyRequired < 0)
+        {
+            // If we ended up with an energy surplus, store it as fat
+            Fat.Put(-energyRequired);
+        }
+        else
+        {
+            // If we don't have enough energy, our health suffers
+            Health -= energyRequired;
+        }
+
+        // We're hungry if our stomach is less than 5% full
+        Hungry = (Stomach.Contents < Stomach.Capacity * 0.05);
+    }
+}
+
+public enum Sex
+{
+    Male,
+    Female,
+    Asexual
+}
+
+[System.Serializable]
+public class Reproduction
+{
+    public Sex Sex;
+    public bool LookingForMate;
+    public float SeasonStart;
+    public float SeasonDuration;
+    public float SeasonEnd;
+    public float SeasonInterval;
+
+    public Color Species;
+
+    public void Tick(float elapsed)
+    {
+        if (Time.time >= SeasonStart)
+        {
+            LookingForMate = true;
+            SeasonEnd = Time.time + SeasonDuration;
+        }
+
+        if (Time.time >= SeasonEnd)
+        {
+            LookingForMate = false;
+            SeasonStart = Time.time + SeasonInterval;
+        }
+    }
+
+    public bool IsSuitableMate(Reproduction candidate)
+    {
+        if (Sex == Sex.Asexual && candidate.Sex != Sex.Asexual) return false;
+        if (Sex != Sex.Asexual && candidate.Sex == Sex.Asexual) return false;
+        if (Sex == candidate.Sex) return false;
+
+        return (SpeciesMatch(Species, candidate.Species, 0.1f));
+    }
+
+    static bool SpeciesMatch(Color s1, Color s2, float tolerance)
+    {
+        var r = Mathf.Abs(s1.r - s2.r);
+        var g = Mathf.Abs(s1.g - s2.g);
+        var b = Mathf.Abs(s1.b - s2.b);
+
+        return r <= tolerance && g <= tolerance && b <= tolerance;
+    }
+
+    static Color MutateSpecies(Color s1, Color s2, float mutation)
+    {
+        var r = (s1.r + s2.r) / 2;
+        var g = (s1.g + s2.g) / 2;
+        var b = (s1.b + s2.b) / 2;
+
+        return new Color(r, g, b);
+    }
+}
+
 public class Mobile : MonoBehaviour
 {
     public Vector3 Destination;
-    public float forwardForce = 3f;
 
-    public float Energy;
-    public float Health;
-    public bool Dead;
-    public bool Hungry;
+    public Body Body = new Body
+    {
+        Stomach = new Stomach { Capacity = 100, Contents = 0 },
+        Fat = new FatReserve { Capacity = float.MaxValue, Contents = 0 },
+        Metabolism = new Metabolism { FatConversionRate = 1, FoodConversionRate = 1 }
+    };
+
+    public Reproduction Reproduction = new Reproduction
+    {
+    };
+
     public GameObject LastFood;
     public float Speed = 0.1f;
+
 
     int nextMinute = 0;
 
     void MinuteTick()
     {
-        if (Dead) return;
+        if (Body.Dead) return;
 
-        if (Energy > 0) Energy--;
-        else Health--;
-
-        if (Health <= 0)
-        {
-            Die();
-            return;
-        }
-
-        Hungry = Energy < 50;
-
-        if (Hungry)
+        if (Body.Hungry)
         {
             if (LastFood != null) Destination = LastFood.transform.position;
         }
 
         if (Arrived())
         {
-            if (Hungry && LastFood != null)
+            if (Body.Hungry && LastFood != null)
             {
-                var eaten = LastFood.GetComponent<Food>().Eat(10f);
-                Energy += eaten;
+                var eaten = LastFood.GetComponent<Food>().Eat(Body.Stomach.Capacity-Body.Stomach.Contents);
+                Body.Stomach.Ingest(eaten);
                 if (eaten <= 0)
                 {
                     LastFood = null;
@@ -56,7 +209,7 @@ public class Mobile : MonoBehaviour
 
     private void Die()
     {
-        Dead = true;
+        Body.Dead = true;
         renderer.material.color = Color.black;
         rigidbody.isKinematic = false; // So we fall over and roll down hill :)
         Destroy(gameObject, 30);
@@ -64,14 +217,22 @@ public class Mobile : MonoBehaviour
 
     void Tick()
     {
-        if (Dead) return;
+        if (Body.Dead) return;
+
+        Body.Tick(Time.deltaTime);
+
+        if (Body.Health <= 0)
+        {
+            Die();
+            return;
+        }
+
+        Reproduction.Tick(Time.deltaTime);
 
         transform.LookAt(Destination);
 
         if (!Arrived())
         {
-            //rigidbody.AddForce(transform.forward * forwardForce);
-
             var newPos = PlaceOnTerrain(transform.position + transform.forward * Speed * Time.deltaTime, 1f);
             transform.position = newPos;
         }
